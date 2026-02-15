cb7f237a52c12d97ea347e775328c3aa3cf031e7
renpet <renpet@gmail.com>
2026-02-15 17:25:14 +0900

    errors fix

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -46,7 +46,7 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen overflow-hidden">
             <Sidebar />
-            <div className="flex flex-col flex-1 w-full lg:pl-64 h-full relative">
+            <div className="flex flex-col flex-1 h-full relative overflow-hidden">
                 <Header />
                 <main className="flex-1 overflow-y-auto p-4 md:p-6 pb-20 lg:pb-6">
                     <div className="max-w-7xl mx-auto">
